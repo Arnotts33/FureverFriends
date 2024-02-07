e1ded55330fd84fbbecc430967c1ef48ecdb5a30
JuliaKrueger <jkrueger557@yahoo.com>
2024-02-07 14:37:14 +0100

    breeds backend

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :profiles, only: [:show, :new, :create, :edit, :update]
+  resources :breeds, only: [:index]
 end
