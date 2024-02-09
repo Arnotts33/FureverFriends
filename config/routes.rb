c0b798cca45cfb451330db3339cc6df7f0e51e9a
Arnaud Lahaut <arnaudlht@gmail.com>
2024-02-09 12:44:44 +0100

    profile and dashboard done with working links and user profile picture showing on all pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   root to: "pages#home"
   get 'dashboard', to: 'dashboard#index'
-  get 'profiles', to: 'profiles#update'
   resources :quizzes, only: [:index] do
     post 'submit', on: :collection
   end
-  resources :breeds, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
