04171bac290da19a1f6565f19d06baa7cbdb8899
Arnaud Lahaut <arnaudlht@gmail.com>
2024-02-15 14:46:37 +0100

    bug fixed and deploy to heroku now for test

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module FureverFriends
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
